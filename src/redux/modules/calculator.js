4a7d6e194364d33a8143c297ce45f912419d1ee9
wonee09 <wonee09@gmail.com>
2024-02-05 18:48:11 +0900

    feat: ex1 완성

---
+++ src/redux/modules/calculator.js
@@ -0,0 +1,10 @@
+// 초기 상태값
+const initialState = {
+  number: 0,
+};
+
+// 리듀서
+const calculator = (state = initialState, action) => {};
+
+// 모듈파일에서는 리듀서를 export default 한다.
+export default calculator;
